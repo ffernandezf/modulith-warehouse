5e60a780e5abd6c0698b98e7bbe4df2dfccfe533
Felix Fernandez <ffernandezf@viewnext.com>
2024-06-23 14:30:17 +0200

    avoid contextLoad tests to improve speed and focus in modularity tests

--- src/test/java/com/personal/warehouse/WarehouseApplicationTests.java
+++ src/test/java/com/personal/warehouse/WarehouseApplicationTests.java
@@ -1,13 +1,12 @@
 package com.personal.warehouse;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WarehouseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
