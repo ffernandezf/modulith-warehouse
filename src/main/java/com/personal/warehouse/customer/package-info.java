02dc2dea3ab960dffcd491b2da29b912b7b2e500
Felix Fernandez <ffernandezf@viewnext.com>
2024-06-24 13:31:29 +0200

    package-info files organized in the same way

--- src/main/java/com/personal/warehouse/customer/package-info.java
+++ src/main/java/com/personal/warehouse/customer/package-info.java
@@ -1,8 +1,3 @@
-/**
- * The logical application module inventory implemented as a single-package module. Allows to hide application components inside the module by using package scoped types.
- *
- * @see example.inventory.InventoryInternal
- */
 @org.springframework.modulith.ApplicationModule
 
 package com.personal.warehouse.customer;
